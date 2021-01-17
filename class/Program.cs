eab1372dba8555b48cc0fcb5c6be9f609e42b621
Elif Nur elaldı <elif.elaldi@gmail.com>
2021-01-17 13:06:54 +0300

    16 ocak ders sonu

---
+++ class/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace class
+{
+    class Program
+{
+    static void Main(string[] args)
+    {
+        string adi = "Engin";
+        int yas = 36;
+
+        Kurs kurs1 = new Kurs();
+        kurs1.KursAdi = "C#";
+        kurs1.Egitmen = "Engin Demiroğ";
+
+
+        Console.WriteLine("Hello World!");
+    }
+}
+
+
+class Kurs
+{
+    public string KursAdi { get; set; }
+
+}
+}
